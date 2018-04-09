6f83613e18910802402b5e586e2295b30a4c52d6
PowerDos <543541941@qq.com>
2018-04-09 22:46:00 +0800

    add vue router

---
+++ Vue/vueRouterDemo/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  color: #42b983;
+}
+</style>
---
+++ Vue/vueRouterDemo/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'I am Home'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  color: #42b983;
+}
+</style>
