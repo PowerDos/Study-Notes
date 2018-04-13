68f94cd514fb763f1b286ccb60c09e040023797b
PowerDos <543541941@qq.com>
2018-04-13 11:09:07 +0800

    add vue VuexDemo

---
+++ Vue/vuexDemo/src/components/Count.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>{{ msg }}</h3>
+    <hr>
+    <h3>{{ $store.state.count }}</h3>
+    <button @click="$store.commit('add')">+</button>
+    <button @click="$store.commit('reduce')">-</button>
+  </div>
+</template>
+
+<script>
+import store from '@/vuex/store'
+export default {
+  name: 'Count',
+  data () {
+    return {
+      msg: 'Vuex 计数器'
+    }
+  },
+  store
+}
+</script>
